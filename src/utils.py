1a99e4c7d67ca7e50daf8ac0d2f814abefc197b2
Chirag Verma <chirag.yep@gmail.com>
2025-02-15 12:52:22 +0530

    Model training finished, gitignore updated

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import dill
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 
 # dill is the libray used to create the pkl file
@@ -16,5 +17,29 @@ def save_object(file_path, obj):
 
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}  
+
+        for model_name, model in models.items():  # Unpack key-value pairs correctly
+            # Train the model
+            model.fit(X_train, y_train)
+
+            # Make predictions
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            # Calculate r2 scores
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            # Store the test score in the report dictionary
+            report[model_name] = test_model_score  
+
+        return report  # Ensure the function returns the report
+
     except Exception as e:
         raise CustomException(e, sys)
